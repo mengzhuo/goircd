491e6602aa27f2266c8bc2c6d009b8cd658f440e
Sergey Matveev <stargrave@stargrave.org>
2015-12-31 20:23:21 +0300

    Raise copyright years

--- common_test.go
+++ common_test.go
@@ -1,6 +1,6 @@
 /*
 goircd -- minimalistic simple Internet Relay Chat (IRC) server
-Copyright (C) 2014-2015 Sergey Matveev <stargrave@stargrave.org>
+Copyright (C) 2014-2016 Sergey Matveev <stargrave@stargrave.org>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
